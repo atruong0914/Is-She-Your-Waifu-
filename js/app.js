3da8afb00432bccd2bc921d1c76fc2cd3ef309ec
annika truong <annikatruongt@gmail.com>
2021-11-19 09:03:39 -0800

    home page done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// home page
+// **jquery**
+// how to play btn - select id - click - toggle display
+$('#btn-rules').click(function(){
+    $('.how-to-play').toggle();
+});
+// play btn - select id - click - go to waifu.html
+$('#btn-play').click(function(){
+    window.location.href = 'waifu.html';
+});
+
+// waifu page
+// **jquery**
